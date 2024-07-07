504c9c8539d717e78ed8c6606e36636353a4c30e
Sophia Okito <okitooghenetega@gmail.com>
2024-07-07 22:23:14 +0100

    Teaching git

---
+++ src/main/java/org/example/Day1.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Day1 {
+    public static void main(String[] args) {
+        //print hello world to the console
+        System.out.println("Hello world!");
+
+
+
+    }
+}
